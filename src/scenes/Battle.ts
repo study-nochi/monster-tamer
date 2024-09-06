7196d4855312992ee6a2d1629079efe7a584937c
Nochi <kthrkdals@gmail.com>
2024-09-06 18:05:39 +0900

    Ep. 8 - Battle Scene Layout Part 3

--- src/scenes/Battle.ts
+++ src/scenes/Battle.ts
@@ -1,7 +1,9 @@
 import { Scene } from "phaser";
 import { SCENE_KEY } from "../constants/scene";
 import {
+  BATTLE_ASSET_KEYS,
   BATTLE_BACKGROUND_ASSET_KEYS,
+  HEALTH_BAR_ASSET_KEYS,
   MONSTER_ASSET_KEYS,
 } from "../constants/asset";
 
@@ -25,5 +27,86 @@ export class Battle extends Scene {
     // render out the player and enemy monsters
     this.add.image(768, 144, MONSTER_ASSET_KEYS.CARNODUSK, 0);
     this.add.image(256, 316, MONSTER_ASSET_KEYS.IGUANIGNITE, 0).setFlipX(true);
+
+    // render out the health bars
+    const playerMonsterName = this.add.text(
+      30,
+      20,
+      MONSTER_ASSET_KEYS.IGUANIGNITE,
+      {
+        color: "#7E3D3F",
+        fontSize: "32px",
+      }
+    );
+    this.add.container(556, 318, [
+      this.add
+        .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
+        .setOrigin(0),
+      playerMonsterName,
+      this.#createHealth(34, 34),
+      this.add.text(playerMonsterName.width + 35, 23, "L5", {
+        color: "#ED474B",
+        fontSize: "28px",
+      }),
+      this.add.text(30, 55, "HP", {
+        color: "#FF6505",
+        fontSize: "24px",
+        fontStyle: "italic",
+      }),
+      this.add
+        .text(443, 80, "25/25", {
+          color: "#7E3D3F",
+          fontSize: "16px",
+        })
+        .setOrigin(1, 0),
+    ]);
+
+    // render out the enemy health bars
+    const enemyMonsterName = this.add.text(
+      30,
+      20,
+      MONSTER_ASSET_KEYS.CARNODUSK,
+      {
+        color: "#7E3D3F",
+        fontSize: "32px",
+      }
+    );
+    this.add.container(0, 0, [
+      this.add
+        .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
+        .setOrigin(0)
+        .setScale(1, 0.8),
+      enemyMonsterName,
+      this.#createHealth(34, 34),
+      this.add.text(enemyMonsterName.width + 35, 23, "L5", {
+        color: "#ED474B",
+        fontSize: "28px",
+      }),
+      this.add.text(30, 55, "HP", {
+        color: "#FF6505",
+        fontSize: "24px",
+        fontStyle: "italic",
+      }),
+      this.add.text(443, 80, "25/25", {
+        color: "#7E3D3F",
+        fontSize: "16px",
+      }),
+    ]);
+  }
+
+  #createHealth(x: number, y: number) {
+    const scaleY = 0.7;
+    const leftCap = this.add
+      .image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP)
+      .setOrigin(0, 0.5)
+      .setScale(1, scaleY);
+    const middle = this.add
+      .image(leftCap.x + leftCap.width, y, HEALTH_BAR_ASSET_KEYS.MIDDLE)
+      .setOrigin(0, 0.5);
+    middle.displayWidth = 360;
+    const rightCap = this.add
+      .image(middle.x + middle.displayWidth, y, HEALTH_BAR_ASSET_KEYS.RIGHT_CAP)
+      .setOrigin(0, 0.5);
+    return this.add.container(x, y, [leftCap, middle, rightCap]);
   }
 }
