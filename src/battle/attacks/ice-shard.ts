5af240ffcb04d2e7c5cec011c82f23feffa571fc
Nochi <kthrkdals@gmail.com>
2024-09-23 03:05:21 +0900

    Ep. 34 - Attack Animations Part 2 but not play

--- src/battle/attacks/ice-shard.ts
+++ src/battle/attacks/ice-shard.ts
@@ -7,16 +7,69 @@ export class IceShard extends Attack {
 
   constructor(scene: Phaser.Scene, position: Coordinate) {
     super(scene, position);
+    // create animations
+    this._scene.anims.create({
+      key: ATTACK_ASSET_KEYS.ICE_SHARD,
+      frames: this._scene.anims.generateFrameNumbers(
+        ATTACK_ASSET_KEYS.ICE_SHARD
+      ),
+      frameRate: 8,
+      repeat: 0,
+      delay: 0,
+    });
 
+    this._scene.anims.create({
+      key: ATTACK_ASSET_KEYS.ICE_SHARD_START,
+      frames: this._scene.anims.generateFrameNumbers(
+        ATTACK_ASSET_KEYS.ICE_SHARD_START
+      ),
+      frameRate: 8,
+      repeat: 0,
+      delay: 0,
+    });
+
+    // create game objects
     this._attackGameObject = this._scene.add
       .sprite(
         this._position.x,
         this._position.y,
         ATTACK_ASSET_KEYS.ICE_SHARD,
-        4
+        5
       )
       .setOrigin(0.5)
       .setScale(4)
       .setAlpha(0);
+  }
+
+  playAnimation(callback?: () => void): void {
+    if (this._isAnimationPlaying) {
+      return;
+    }
+
+    this._isAnimationPlaying = true;
+    this._attackGameObject.setAlpha(1);
+
+    this._attackGameObject.play(ATTACK_ASSET_KEYS.ICE_SHARD_START);
+
+    this._attackGameObject.once(
+      Phaser.Animations.Events.ANIMATION_COMPLETE_KEY +
+        ATTACK_ASSET_KEYS.ICE_SHARD_START,
+      () => {
+        this._attackGameObject.play(ATTACK_ASSET_KEYS.ICE_SHARD);
+      }
+    );
+
+    this._attackGameObject.once(
+      Phaser.Animations.Events.ANIMATION_COMPLETE_KEY +
+        ATTACK_ASSET_KEYS.ICE_SHARD,
+      () => {
+        this._isAnimationPlaying = false;
+        this._attackGameObject.setAlpha(0).setFrame(0);
+
+        if (callback) {
+          callback();
+        }
+      }
+    );
   }
 }
--- src/scenes/Preload.ts
+++ src/scenes/Preload.ts
@@ -28,97 +28,102 @@ export class Preload extends Scene {
     const dataAssetPath = "assets/data";
     const pimenAssetPath = "assets/pimen";
 
-    // battle backgrounds
-    this.load.image(
-      BATTLE_BACKGROUND_ASSET_KEYS.FOREST,
-      `${monsterTamerAssetPath}/battle-backgrounds/forest-background.png`
-    );
+    Promise.all([
+      // battle backgrounds
+      this.load.image(
+        BATTLE_BACKGROUND_ASSET_KEYS.FOREST,
+        `${monsterTamerAssetPath}/battle-backgrounds/forest-background.png`
+      ),
 
-    // battle assets
-    this.load.image(
-      BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND,
-      `${kenneysAssetPath}/ui-space-expansion/custom-ui.png`
-    );
+      // battle assets
+      this.load.image(
+        BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND,
+        `${kenneysAssetPath}/ui-space-expansion/custom-ui.png`
+      ),
 
-    // health bar assets
-    this.load.image(
-      HEALTH_BAR_ASSET_KEYS.RIGHT_CAP,
-      `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_right.png`
-    );
-    this.load.image(
-      HEALTH_BAR_ASSET_KEYS.MIDDLE,
-      `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_mid.png`
-    );
-    this.load.image(
-      HEALTH_BAR_ASSET_KEYS.LEFT_CAP,
-      `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_left.png`
-    );
+      // health bar assets
+      this.load.image(
+        HEALTH_BAR_ASSET_KEYS.RIGHT_CAP,
+        `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_right.png`
+      ),
+      this.load.image(
+        HEALTH_BAR_ASSET_KEYS.MIDDLE,
+        `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_mid.png`
+      ),
+      this.load.image(
+        HEALTH_BAR_ASSET_KEYS.LEFT_CAP,
+        `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_left.png`
+      ),
 
-    this.load.image(
-      HEALTH_BAR_ASSET_KEYS.RIGHT_CAP_SHADOW,
-      `${kenneysAssetPath}/ui-space-expansion/barHorizontal_shadow_right.png`
-    );
-    this.load.image(
-      HEALTH_BAR_ASSET_KEYS.MIDDLE_SHADOW,
-      `${kenneysAssetPath}/ui-space-expansion/barHorizontal_shadow_mid.png`
-    );
-    this.load.image(
-      HEALTH_BAR_ASSET_KEYS.LEFT_CAP_SHADOW,
-      `${kenneysAssetPath}/ui-space-expansion/barHorizontal_shadow_left.png`
-    );
+      this.load.image(
+        HEALTH_BAR_ASSET_KEYS.RIGHT_CAP_SHADOW,
+        `${kenneysAssetPath}/ui-space-expansion/barHorizontal_shadow_right.png`
+      ),
+      this.load.image(
+        HEALTH_BAR_ASSET_KEYS.MIDDLE_SHADOW,
+        `${kenneysAssetPath}/ui-space-expansion/barHorizontal_shadow_mid.png`
+      ),
+      this.load.image(
+        HEALTH_BAR_ASSET_KEYS.LEFT_CAP_SHADOW,
+        `${kenneysAssetPath}/ui-space-expansion/barHorizontal_shadow_left.png`
+      ),
 
-    // monster assets
-    this.load.image(
-      MONSTER_ASSET_KEYS.IGUANIGNITE,
-      `${monsterTamerAssetPath}/monsters/iguanignite.png`
-    );
-    this.load.image(
-      MONSTER_ASSET_KEYS.CARNODUSK,
-      `${monsterTamerAssetPath}/monsters/carnodusk.png`
-    );
+      // monster assets
+      this.load.image(
+        MONSTER_ASSET_KEYS.IGUANIGNITE,
+        `${monsterTamerAssetPath}/monsters/iguanignite.png`
+      ),
+      this.load.image(
+        MONSTER_ASSET_KEYS.CARNODUSK,
+        `${monsterTamerAssetPath}/monsters/carnodusk.png`
+      ),
 
-    // ui assets
-    this.load.image(
-      UI_ASSET_KEYS.CURSOR,
-      `${monsterTamerAssetPath}/ui/cursor.png`
-    );
+      // ui assets
+      this.load.image(
+        UI_ASSET_KEYS.CURSOR,
+        `${monsterTamerAssetPath}/ui/cursor.png`
+      ),
 
-    // load json data
-    this.load.json(DATA_ASSET_KEYS.ATTACKS, `${dataAssetPath}/attacks.json`);
+      // load json data
+      this.load.json(DATA_ASSET_KEYS.ATTACKS, `${dataAssetPath}/attacks.json`),
 
-    // load custom fonts
-    this.load.addFile(
-      new WebFontFileLoader(this.load, [KENNEY_FUTURE_NARROW_FONT_NAME])
-    );
+      // load custom fonts
+      this.load.addFile(
+        new WebFontFileLoader(this.load, [KENNEY_FUTURE_NARROW_FONT_NAME])
+      ),
 
-    // load attack assets
-    this.load.spritesheet(
-      ATTACK_ASSET_KEYS.ICE_SHARD,
-      `${pimenAssetPath}/ice-attack/active.png`,
-      {
-        frameWidth: 32,
-        frameHeight: 32,
-      }
-    );
-    this.load.spritesheet(
-      ATTACK_ASSET_KEYS.ICE_SHARD_START,
-      `${pimenAssetPath}/ice-attack/start.png`,
-      {
-        frameWidth: 32,
-        frameHeight: 32,
-      }
-    );
-    this.load.spritesheet(
-      ATTACK_ASSET_KEYS.SLASH,
-      `${pimenAssetPath}/slash.png`,
-      {
-        frameWidth: 48,
-        frameHeight: 48,
-      }
-    );
+      // load attack assets
+      this.load.spritesheet(
+        ATTACK_ASSET_KEYS.ICE_SHARD,
+        `${pimenAssetPath}/ice-attack/active.png`,
+        {
+          frameWidth: 32,
+          frameHeight: 32,
+        }
+      ),
+      this.load.spritesheet(
+        ATTACK_ASSET_KEYS.ICE_SHARD_START,
+        `${pimenAssetPath}/ice-attack/start.png`,
+        {
+          frameWidth: 32,
+          frameHeight: 32,
+        }
+      ),
+      this.load.spritesheet(
+        ATTACK_ASSET_KEYS.SLASH,
+        `${pimenAssetPath}/slash.png`,
+        {
+          frameWidth: 48,
+          frameHeight: 48,
+        }
+      ),
+    ]).then(() => {
+      console.log(`[${Preload.name}:preload] assets loaded`);
+    });
   }
 
   create() {
+    console.log(`[${Preload.name}:create] invoked`);
     this.scene.start(SCENE_KEYS.BATTLE_SCENE);
   }
 }
